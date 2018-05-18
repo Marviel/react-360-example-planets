f23f885c348f7fa82a1d48dfe2531d981b5414d6
Marviel <lukebechtel4@gmail.com>
2018-05-17 22:40:28 -0400

    add complex orbitals

--- index.js
+++ index.js
@@ -1,57 +1,155 @@
 import React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Scene,
-  Box,
-  Cylinder,
-  Sphere,
-  AmbientLight,
-  PointLight,
-  VrButton,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Scene,
+    Box,
+    Cylinder,
+    Sphere,
+    AmbientLight,
+    PointLight,
+    VrButton,
 } from 'react-360';
+
+import _ from "underscore";
+
+var initialSpheresFirst  = [{
+                            key: uuidv4(),
+                            radius: .2,
+                            color: randomHex(),
+                            translate: [0, 0, -3],
+                            rotation: 10,
+                            spheres: [
+                                {
+                                    key: uuidv4(),
+                                    radius: .1,
+                                    color: randomHex(),
+                                    translate: [0,0,-1],
+                                    rotation: 2,
+                                    spheres: [
+                                        {
+                                            key: uuidv4(),
+                                            radius: .05,
+                                            color: randomHex(),
+                                            translate: [0,0,-.5],
+                                            rotation: 2,
+                                            spheres: []
+                                        }
+                                    ]
+
+                                }
+                            ]
+                        }
+            ];
 
 function seededRandom(seed) {
     var x = Math.sin(seed) * 10000;
     return x - Math.floor(x);
 }
 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function randomHex(toHash) {
+    toHash = toHash ? toHash : Math.random()
+    return '#'+seededRandom(toHash).toString(16).substr(-6);
+}
+
+function generateInitialSpheres1({radius, color, translate, rotation, depth}) {
+    console.log("generateInitialSpheres1", depth)
+    return depth > 0 ? [{
+                            key: uuidv4(),
+                            radius,
+                            color,
+                            translate,
+                            rotation,
+                            spheres: generateInitialSpheres1({radius: radius-radius/3, 
+                                                              color: randomHex(),
+                                                              translate: [translate[0],translate[1],translate[2]-translate[2]/2],
+                                                              rotation,
+                                                              depth: depth -1})
+                        }] : []
+}
+
+const PlanetarySphere = ({key, radius, translate, rotation, color, spheres}) => (
+    <Sphere
+        key={key}
+        radius={radius}
+        lit={true}
+        widthSegments={40}
+        heightSegments={40}
+        style={{
+            color: color,
+            transform: [{translate: translate}, 
+                        {rotateX: rotation[0]},
+                        {rotateY: rotation[1]},
+                        {rotateZ: rotation[2]}],
+        }}
+        mouse
+    >
+    {spheres.map((val) =>{
+        return val ? <PlanetarySphere key={val.key}
+                                      radius={val.radius}
+                                      translate={val.translate}
+                                      color={val.color}
+                                      rotation={val.rotation}
+                                      spheres={val.spheres}/> : <div/>
+     })
+    }
+    </Sphere> 
+);
+
+
 
 export default class planetaryR360 extends React.Component {
-  constructor(props){
-  	super(props);
-	this.state = {
-		rotation: 0,
-		rotation_medium: 0,
-		rotation_small: 0,
-		segments: 4,
-		increasingSegments: true,
-		numSpheres: 1,
-		baseSeed: 10,
-		increasingSpheres: true
-	}
-	
-	window.setInterval(()=>{this.changeRotations()}, 10);
-  }
+    constructor(props){
+        super(props);
+
+        baseSphereUuid = uuidv4()
+
+        this.state = {
+            xRotDelta: 1,
+            yRotDelta: 1,
+            zRotDelta: 1,
+            segments: 4,
+            increasingSegments: true,
+            numSpheres: 1,
+            baseSeed: 10,
+            increasingSpheres: true,
+            spheres: generateInitialSpheres1(
+                        {radius: .2,
+                        color: randomHex(),
+                        translate: [0, 0, -5],
+                        rotation: [0,10,0],
+                        depth: 10})
+        }
+
+
+        console.log(this.state.spheres)
+        window.setInterval(()=>{this.changeRotations()}, 10);
+    }
 
   changeRotations = () =>{
-	var increasingSegments = this.state.increasingSegments;
-	if (this.state.segments > 500){
-		increasingSegments = false;
-	}
-	if (this.state.segments <= 5){
-		increasingSegments = true;
-	}
-
-	this.setState({
-		rotation: this.state.rotation + .25,
-		rotation_medium: this.state.rotation_medium + .5,
-		rotation_small: this.state.rotation_small + 3,
-		segments: this.state.increasingSegments ? this.state.segments + 1 : this.state.segments - 1,
-		increasingSegments,
+    const spheres = [this.calculateChangeTree(this.state.spheres[0])];
+    this.setState({
+        spheres
 	})
+  }
+
+  calculateChangeTree = (sphere)=>{
+    return { 
+                ...sphere,
+                rotation: [sphere.rotation[0] + this.state.xRotDelta, 
+                           sphere.rotation[1] + this.state.yRotDelta, 
+                           sphere.rotation[2] + this.state.zRotDelta],
+                spheres: sphere.spheres.map((v) => this.calculateChangeTree(v))
+           }
   }
 
   changeMoons = () =>{
@@ -72,55 +170,88 @@ export default class planetaryR360 extends React.Component {
 	})
   }
 
+  rotationDeltaOnChange = (ind, amt) =>{
+    var newState = {};
+    if (ind == 0){
+        newState.xRotDelta = this.state.xRotDelta + amt;
+    }
+    if (ind == 1){
+        newState.yRotDelta = this.state.yRotDelta + amt;
+    }
+    if (ind == 2){
+        newState.zRotDelta = this.state.zRotDelta + amt;
+    }
+    this.setState(newState);
+  }
+
   
 	
   render() {
-	// Create Sphere objects
-    var spheres = Array.from(new Array(this.state.numSpheres), 
-					(x, i)=>{
-									var color = '#'+seededRandom(i+this.state.baseSeed).toString(16).substr(-6);
-									var rads = (Math.PI*2)/this.state.numSpheres * i;
-									var translate = [.3*Math.sin(rads),0,.3*Math.cos(rads)]
-									return <Sphere
-									radius={.05}
-									lit={true}
-									style={{
-									  color,
-									  transform: [{translate}, {rotateY: this.state.rotation_small}],
-									}}
-									onInput={(ev)=>{ev.stopPropagation()}}
-								/>});
+    const baseSphere = this.state.spheres[0];
 	return (
-	  <Scene style={{transform:[{rotateY: this.state.rotation}]}}>
+	  <Scene>
       <View>
 		<AmbientLight intensity={1.0} color={'#ffffff'} />
 		<PointLight
 	  	 intensity={0.4}
 	 	 style={{transform: [{translate: [0, 4, -1]}]}}
 		/>
-		<Sphere
-			radius={.2}
-			lit={true}
-			style={{
-			  color: '#96deFF',
-			  transform: [{translate: [-0,-0,-3]}, {rotateY: this.state.rotation}],
-			}}
-			mouse
-			onInput={(ev)=>{console.log("input!", ev.nativeEvent.inputEvent)}}
-      	>
-			<Sphere
-				radius={.1}
-				lit={true}
-				style={{
-				  color: '#96AA00',
-				  transform: [{translate: [-0,-0,-1]}, {rotateX: this.state.rotation_medium}],
-				}}
-				onInput={(ev)=>{ev.nativeEvent.inputEvent.action == "down" ? this.changeMoons(): null;
-								ev.stopPropagation()}}
-			>
-				{spheres}
-			</Sphere>
-		</Sphere>
+		
+        <PlanetarySphere radius={baseSphere.radius}
+                         color={baseSphere.color}
+                         translate={baseSphere.translate}
+                         rotation={baseSphere.rotation}
+                         spheres={baseSphere.spheres}/>
+        <Text
+          style={{
+                          backgroundColor: '#777899',
+                              fontSize: 0.2,
+                              layoutOrigin: [0.5, 0.5],
+                              paddingLeft: 0.2,
+                              paddingRight: 0.2,
+                              textAlign: 'center',
+                              textAlignVertical: 'center',
+                              transform: [{translate: [-2, 0, -3]},
+                                          {rotateY: 50}],
+                            }}
+          onInput={()=>{this.rotationDeltaOnChange(0,1)}}
+         >
+            X Up
+         </Text>
+         <Text
+            style={{
+                          backgroundColor: '#7778BB',
+                              fontSize: 0.2,
+                              layoutOrigin: [0.5, 0.5],
+                              paddingLeft: 0.2,
+                              paddingRight: 0.2,
+                              textAlign: 'center',
+                              textAlignVertical: 'center',
+                              transform: [{translate: [-3, 0, -3]},
+                                          {rotateY: 50}],
+                            }}
+  
+            onInput={()=>{this.rotationDeltaOnChange(1,1)}}
+          >
+            Y Up
+          </Text>
+        <Text
+          style={{
+                          backgroundColor: '#7778FF',
+                              fontSize: 0.2,
+                              layoutOrigin: [0.5, 0.5],
+                              paddingLeft: 0.2,
+                              paddingRight: 0.2,
+                              textAlign: 'center',
+                              textAlignVertical: 'center',
+                              transform: [{translate: [-4, 0, -3]},
+                                          {rotateY: 50}],
+                            }}
+          onInput={()=>{this.rotationDeltaOnChange(2,1)}}
+         >
+            Z Up
+         </Text>
+
       </View>
 	  </Scene>
     );
